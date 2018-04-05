25542572057a4dfe212a2f7d4f26564e02cc6c40
Rob <36937649+rmccarth@users.noreply.github.com>
2018-04-05 15:16:33 -0400

    ~20,000 titles and viewer counts; DRY'r code

--- collectTwitch.py
+++ collectTwitch.py
@@ -1,65 +1,61 @@
 import requests  
 import json
+import time
+
 #text analytics api
 
 def main():
-    key_value = get_twitch()                                # Take the value given by the "cursor" field in the JSON response and store it in key_value. 
-    while key_value != None:                                # Trying to iterate here in order to pull down all pages based on the cursor field number.
-        get_next_page(key_value)                            # When the cursor field is not longer populated, we don't have to query the API anymore (we willl have all data)
-             
+    
+    page_number = None
+    page_number = get_twitch(page_number)
+    if page_number == 'done':
+        print('...All data printed')
+        return
+    else:                                    # Take the value given by the "cursor" field in the JSON response and store it in key_value. 
+        while page_number != None:
+            time.sleep(3)                                # Trying to iterate here in order to pull down all pages based on the cursor field number.
+            page_number = get_twitch(page_number)                            # When the cursor field is not longer populated, we don't have to query the API anymore (we will have all data)
+            if page_number == 'done':
+                print('...All data printed')
+                return
 
-#def create_json_library():
-
-def get_twitch():
+def get_twitch(after_value):
     
     URL = 'https://api.twitch.tv/helix/streams'                         
     headers = {"Client-ID": "t8ag4decgvfrpm3foi7mxb3rgqgleg"}
-    params = {"language": "en"}
+    if after_value is None:
+        params = {"language": "en"}
+    else:
+        params = {"after": after_value, "language": "en"}
     response = requests.get(URL, headers=headers, params=params)                   # Standard requests-module request for the API. 
-    print(response.text)
-    parsed_json = json.loads(response.text)                         # Stores the response in JSON dictionary format. 
-    page_number = parsed_json['pagination']['cursor']               # Storing the cursor value here (JSON dictionary lookup)
+    parsed_json = json.loads(response.text)                      # Stores the response in JSON dictionary format. 
+    after_value = parsed_json['pagination']['cursor']               # Storing the cursor value here (JSON dictionary lookup)
     
     storage = []
-    for item in parsed_json['data']:
-        if 'title' and 'viewer_count' in item:
-            #print(item['title'] + ' ' + str(item['viewer_count']))
-            storage.append(item['title'])
-            with open("test.txt", 'a', encoding='utf-8') as dirtyfile:
-                dirtyfile.write(item['title'])
-            storage.append(item['viewer_count'])
-            with open("test.txt",'a',encoding = 'utf-8') as dirtyfile:
-                dirtyfile.write('\t')
-                dirtyfile.write(str(item['viewer_count']))
-                dirtyfile.write('\n')
-   
-    return page_number
+    if response.status_code != '429':  
+        with open("test.txt", 'a', encoding='utf-8') as dirtyfile:                         # fix with
+            for item in parsed_json['data']:
+                
+                if 'title' and 'viewer_count' in item:
+                    print('found title and viewer_count')
 
+                    title = item['title'].replace('\t', "")
+                    if title not in storage:
+                    
+                        storage.append(title)
+                    
+                        dirtyfile.write(title)
+                    
+                        storage.append(item['viewer_count'])
+                   
+                        dirtyfile.write('\t')
+                        dirtyfile.write(str(item['viewer_count']))
+                        dirtyfile.write('\n')
 
-def get_next_page(page_number):                                     # Pretty much the same stuff here but passing the cursor number in the paramaters
-                                                                    # In order to make sure we are querying the next page. 
-    URL = 'https://api.twitch.tv/helix/streams'
-    headers = {"Client-ID": "t8ag4decgvfrpm3foi7mxb3rgqgleg"}
-    params = {"after": page_number, "language": "en"}
-    response = requests.get(URL, headers=headers, params=params)    
-    print(response.status_code)
-
-    parsed_json = json.loads(response.text)
-    iterative_page = parsed_json['pagination']['cursor']
-    storage = []
-    for item in parsed_json['data']:
-        if 'title' and 'viewer_count' in item:
-            #print(item['title'] + ' ' + str(item['viewer_count']))
-            storage.append(item['title'])
-            with open("test.txt", 'a', encoding='utf-8') as dirtyfile:
-                dirtyfile.write(item['title'])
-            storage.append(item['viewer_count'])
-            with open("test.txt",'a',encoding = 'utf-8') as dirtyfile:
-                dirtyfile.write('\t')
-                dirtyfile.write(str(item['viewer_count']))
-                dirtyfile.write('\n')
-
-    return iterative_page
+    if response.status_code == '429':
+        return 'done'
+    else:
+        return after_value
 
 main()
 
